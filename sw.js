702953107251ba57bfd806a6d2f485a1d9d213fb
bridgepoc <105968155+bridgepoc@users.noreply.github.com>
2022-05-21 23:48:53 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const { response } = require("express");
+
+self.addEventListener('install',(event) =>{
+    console.log('installing');
+    event.waitUntil(
+        caches.open("static-cache")
+        .then((cache)=>{
+            return cache.addAll([
+                'css/style.css',
+                'css/bootstrap.min.css',
+                'css/bootstrap.min.css.map',
+                'js/bootstrap.bundle.min.js',
+                'js/bootstrap.bundle.min.js.map',
+                'images/icon512.png',
+            ]);
+        })
+    );
+});
+self.addEventListener('active',(event)=>{
+    console.log('now ready to handle fetch');
+});
+self.addEventListener('fetch', (event)=>{
+    console.log('now fetch');
+    const request = event.request;
+    const url = request.url;
+
+    if(request.headers.get("Accept").includes("text/html")){
+        event.respondWith(
+            caches.match(request)
+            .then(cached_result =>{
+                if(cached_result){
+                    return cached_result;
+                }
+                return fetch(request)
+                .then(response =>{
+                    const copy = response.clone();
+                    event.waitUntil(
+                        caches.open("pages")
+                        .then(cache =>{
+                            return cache.put(request, response);
+                        })
+                    );
+                    return response;
+                });
+            })
+        );
+    }
+});
